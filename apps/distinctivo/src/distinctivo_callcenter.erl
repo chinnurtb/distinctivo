1c91ca89e052b0db5d98b3102ff5cb0789f1cc78
Yurii Rashkovskii <yrashk@gmail.com>
2011-11-19 17:21:55 -0800

    More CallCenter experimentation

--- apps/distinctivo/src/distinctivo_callcenter.erl
+++ apps/distinctivo/src/distinctivo_callcenter.erl
@@ -1,6 +1,6 @@
 -module(distinctivo_callcenter).
--export([outbound_call_reward/2]).
--rules([outbound_call_reward]).
+-export([outbound_call_reward/2, unsatisfied_customer/3]).
+-rules([outbound_call_reward, unsatisfied_customer]).
 %%
 %%
 %% Events
@@ -23,6 +23,11 @@ outbound_call_reward(Engine, {call, outbound, CustomerId, AgentId, Timestamp}) -
     change_score(AgentId, 1),
     Engine.
 
+%% 1. For every unsatisfied customer, you get minus 5 points
+unsatisfied_customer(Engine, {call, outbound, CustomerId, AgentId, Timestamp0},
+                     {customer_agent_rating, CustomerId, AgentId, Rating, Timestamp}) when Rating =< 3 andalso Timestamp0 < Timestamp ->
+    change_score(AgentId, -5),
+    Engine.
 
 %% Implementation details
 change_score(AgentId, By) ->
